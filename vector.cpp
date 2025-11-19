201118ec8e0464b951d1dd17b9fc1d83b33879ad
AtulSingh205 <atulsingh07062004@gmail.com>
2025-11-19 14:46:27 +0530

    update

--- vector.cpp
+++ vector.cpp
@@ -1,11 +1,40 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<char>vec ={'a','b','c','d'};
+//     for(char val:vec){
+//         cout<<val<<endl;
+//     }
+//     return 0;
+
+// }
+
 #include<iostream>
 #include<vector>
-using namespace std;
-int main(){
-    vector<char>vec ={'a','b','c','d'};
-    for(char val:vec){
-        cout<<val<<endl;
+using namespace std ;
+
+ int display(vector<int> &V){
+       for(int i = 0;i<V.size();i++){
+        cout<<V[i]<<" ";
     }
-    return 0;
+    cout<<endl;
+}
+
+int main(){
+    vector<int> V;
+    int  size ,element;
+    cout<<"Enter  the size: ";
+    cin>>size;
+    for(int i = 1;i<size; i++){
+        cout<<"Enter  a element: ";
+        cin>>element;
+        V.push_back(element);
+    }
+    display(V);
+
+    vector<int> :: iterator iter  = V.begin();
+    V.insert(iter+2,5,566);
+    display(V);
 
 }
